ae00fb848598b7b88a38fb9276708a61c5e19bd4
TT7334 <147974412+TT7334@users.noreply.github.com>
2023-12-01 16:44:17 +0100

    Update conclusion.py

--- tabs/conclusion.py
+++ tabs/conclusion.py
@@ -13,11 +13,11 @@ def conclusion():
     texte = """
     - Nous avons pu constater le réchauffement climatique : **c'est une réalité indiscutable**. Cela augmente depuis la révolution industrielle de 1880, et s'accélère à partir de 1975, de manière plus forte encore dans l'hémisphère nord, là où l'industrialisation est la plus intensive.
     - Les émissions de CO2 ont **une forte influence** sur la hausse des températures. Elles sont étroitement liées.
-    - **Nos prédictions ne sont pas optimistes**. D'après notre modèle Prophet, la température moyenne globale augmentera de près de 2°C dans les 50 prochaines années, causées par une augmentation constante des émissions de CO2. Ces résultats rejoignent ceux de la majorité des études disponibles sur le changement climatique.
+    - **Nos prédictions ne sont pas optimistes**. D'après notre modèle Prophet, la température moyenne globale augmentera de près de 2°C dans les 50 prochaines années, causées par une augmentation constante des émissions de CO2 et de toutes les autres émissions de gaz à effet de serre qui ont un lien de corrélation avec ces changements de températures. Ces résultats rejoignent ceux de la majorité des études disponibles sur le changement climatique.
     """
     st.markdown(texte)
 
 
     st.header("Sujets d'ouverture:")
-    st.write("En guise d'ouverture, il serait intéressant de regarder **comment évoluent les différences de températures** en prévoyant des **diminutions drastiques des émissions de CO2.**")
-    st.write("Le comportement des **températures sera probablement différent** et permettra de donner à l'humanité des **objectifs d'emissions** pour vivre de manière pérenne sur notre planète.")
+    st.write("En guise d'ouverture, il serait intéressant de regarder **comment évoluent les différences de températures** en prévoyant des **diminutions (ou augmentations) des émissions de gaz à effet de serre.**")
+    st.write("Le comportement des **températures sera probablement différent** et permettra de donner à l'humanité des **objectifs d'emissions** à atteindre pour vivre de manière pérenne sur notre planète.")
